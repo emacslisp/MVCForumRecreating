7caab1327afdbe6a2674bec8a00b63b4d3d60cd6
emacslisp <java.gdb@gmail.com>
2017-07-23 15:03:46 +1000

    add packages and DbMigration

---
+++ MVCForum/MVCForum.Data/Context/MVCForumContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCForum.Data.Context
+{
+    public class MVCForumContext : DbContext
+    {
+
+    }
+}
---
+++ MVCForum/MVCForum.Data/Migrations/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using MVCForum.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCForum.Data.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCForumContext>
+    {
+
+    }
+}
