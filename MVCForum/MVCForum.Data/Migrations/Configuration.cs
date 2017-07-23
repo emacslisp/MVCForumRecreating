a9f5e476141490c75dd2b280aa72761f05bec461
emacslisp <java.gdb@gmail.com>
2017-07-23 21:16:45 +1000

    add Seed Configuration

--- MVCForum/MVCForum.Data/Migrations/Configuration.cs
+++ MVCForum/MVCForum.Data/Migrations/Configuration.cs
@@ -10,6 +10,15 @@ namespace MVCForum.Data.Migrations
 {
     internal sealed class Configuration : DbMigrationsConfiguration<MVCForumContext>
     {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
 
+        protected override void Seed(MVCForumContext context)
+        {
+
+        }
     }
 }
